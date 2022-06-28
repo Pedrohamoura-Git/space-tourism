55dc3758878c88938b4b3a69b4cd14a41b2e0b4e
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2022-06-27 22:56:42 -0300

    feat(Burger): create component.

---
+++ src/components/Buttons/Burger/index.js
@@ -0,0 +1,13 @@
+import './_index.scss';
+
+export const Burger = () => {
+  return (
+    <button class="hamburger hamburger--criss-cross" type="button">
+        <div class="inner">
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        </div>
+    </button>  
+)
+}
