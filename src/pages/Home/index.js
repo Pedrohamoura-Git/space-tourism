161e3cb8b00d0f69a876671dadffab6f0c818eb7
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2022-06-27 22:16:35 -0300

    refactor(Home): move description outside the h1.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,12 +11,13 @@ export default function Home() {
                 <p className="big">
                     Space
                 </p>
-                <p className="description">
-                    Let’s face it; if you want to go to space, you might as well genuinely go to 
-                    outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                    because we’ll give you a truly out of this world experience!
-                </p>
             </h1>
+            
+            <p className="description">
+                Let’s face it; if you want to go to space, you might as well genuinely go to 
+                outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                because we’ll give you a truly out of this world experience!
+            </p>
 
             <MainButton 
                 name="Explore" 
