d92070f7f245d55f4ec146001ad321f4a6440d7d
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2022-07-11 19:43:24 -0300

    feat: Navbar - add structure for blur effect.

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,37 +8,41 @@ export default function Navbar() {
 
     return (
         <nav className="navbar">
-            <img src={logo} alt="logo" className='logo'/>
-            <span className="divider" />
+            <div className='background'>
+                <div className='content'>
+                    <img src={logo} alt="logo" className='logo'/>
+                    <span className="divider" />
 
-            <Burger />
+                    <Burger />
 
-            <ul className="link-list">
-                <li>
-                    <Link className="link" to="/">
-                        <span className="number">00</span>
-                        <span className='name'>Home</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="link" to="/destination">
-                        <span className="number">01</span>
-                        <span className='name'>destination</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="link" to="/crew">
-                        <span className="number">02</span>
-                        <span className='name'>crew</span>
-                    </Link>
-                </li>
-                <li>
-                    <Link className="link" to="/technology">
-                        <span className="number">04</span>
-                        <span className='name'>technology</span>
-                    </Link>
-                </li>
-            </ul>
+                    <ul className="link-list">
+                        <li>
+                            <Link className="link" to="/">
+                                <span className="number">00</span>
+                                <span className='name'>Home</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/destination">
+                                <span className="number">01</span>
+                                <span className='name'>destination</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/crew">
+                                <span className="number">02</span>
+                                <span className='name'>crew</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/technology">
+                                <span className="number">04</span>
+                                <span className='name'>technology</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </nav>
     )
 }   
