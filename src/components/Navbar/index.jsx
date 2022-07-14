e0228a0487bb3926e12319764364132cb7bc51f7
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2022-07-13 22:12:56 -0300

    refactor: Navbar - .reorganize tags and classes.

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -15,51 +15,50 @@ export default function Navbar() {
 
     return (
         <nav className="navbar">
-            <div className='background'>
-                <div className='content'>
-                    <img src={logo} alt="logo" className='logo'/>
-                    <span className="divider" />
+            <div className='navbar__background-effect' />
+            <div className='navbar__content'>
+                <img src={logo} alt="logo" className='logo'/>
+                <span className="divider" />
 
-                    <Burger
-                        isActive={isActive}
-                        toggleBurger={toggleBurger}
-                    />
-                    
-                    <CSSTransition
-                        in={isActive}
-                        timeout={500}
-                        classNames="slide"
-                        className="transition-wrapper"
-                        unmountOnExit
-                    >
-                        <ul className="link-list">
-                            <li>
-                                <Link className="link" to="/">
-                                    <span className="number">00</span>
-                                    <span className='name'>Home</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="link" to="/destination">
-                                    <span className="number">01</span>
-                                    <span className='name'>destination</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="link" to="/crew">
-                                    <span className="number">02</span>
-                                    <span className='name'>crew</span>
-                                </Link>
-                            </li>
-                            <li>
-                                <Link className="link" to="/technology">
-                                    <span className="number">04</span>
-                                    <span className='name'>technology</span>
-                                </Link>
-                            </li>
-                        </ul>
-                    </CSSTransition>
-                </div>
+                <Burger
+                    isActive={isActive}
+                    toggleBurger={toggleBurger}
+                />
+                
+                <CSSTransition
+                    in={isActive}
+                    timeout={500}
+                    classNames="slide"
+                    className="transition-wrapper"
+                    unmountOnExit
+                >
+                    <ul className="link-list">
+                        <li>
+                            <Link className="link" to="/">
+                                <span className="number">00</span>
+                                <span className='name'>Home</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/destination">
+                                <span className="number">01</span>
+                                <span className='name'>destination</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/crew">
+                                <span className="number">02</span>
+                                <span className='name'>crew</span>
+                            </Link>
+                        </li>
+                        <li>
+                            <Link className="link" to="/technology">
+                                <span className="number">04</span>
+                                <span className='name'>technology</span>
+                            </Link>
+                        </li>
+                    </ul>
+                </CSSTransition>
             </div>
         </nav>
     )
