6a25bd05d7360ce2611fa772141d2eacfcc9bb4a
Pedro Henrique Alves Moura <75505990+Pedrohamoura-Git@users.noreply.github.com>
2022-07-13 22:14:37 -0300

    feawt: Home - add new tags and classes.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,26 +3,30 @@ import './index.scss';
 
 export default function Home() {
     return (
-        <main className="home-container">
-            <h1 className="title">
-                <p className="small">
-                    So, you want to travel to
-                </p>
-                <p className="big">
-                    Space
-                </p>
-            </h1>
-            
-            <p className="description">
-                Let’s face it; if you want to go to space, you might as well genuinely go to 
-                outer space and not hover kind of on the edge of it. Well sit back, and relax 
-                because we’ll give you a truly out of this world experience!
-            </p>
+        <main className="home">
+            <div className="wrapper">
+                <div className='content'>
+                    <h1 className="title">
+                        <p className="small">
+                            So, you want to travel to
+                        </p>
+                        <p className="big">
+                            Space
+                        </p>
+                    </h1>
+                    
+                    <p className="description">
+                        Let’s face it; if you want to go to space, you might as well genuinely go to 
+                        outer space and not hover kind of on the edge of it. Well sit back, and relax 
+                        because we’ll give you a truly out of this world experience!
+                    </p>
 
-            <MainButton 
-                name="Explore" 
-                className="main-button"
-            />
+                    <MainButton 
+                        name="Explore" 
+                        className="main-button"
+                    />
+                </div>
+            </div>
         </main>
     )
 }
